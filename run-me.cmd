e66700ecfda11874ef2d815eff2cf2060999cc3e
Zhiyi Ren <ren-zhiyi@outlook.com>
2020-10-25 10:27:39 +0800

    edit run-me.cmd&run-me.sh

--- run-me.cmd
+++ run-me.cmd
@@ -1,3 +1,3 @@
 set /p Please enter the server.:
 rem The configuration file is in the config folder and the file name is server name.json
-./v2ray -config ./config/%server%.json
+./giggle -config ./config/%server%.json
--- run-me.sh
+++ run-me.sh
@@ -2,4 +2,4 @@
 echo "Please enter the server."
 read server
 #The configuration file is in the config folder and the file name is server name.json
-./v2ray -config ./config/$server.json
+./giggle -config ./config/$server.json
